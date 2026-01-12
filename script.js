51f2adb53b1b16b13f305dba5adbbc3b441d3ff6
artjiyr <106913775+artjiyr@users.noreply.github.com>
2026-01-12 01:20:34 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -18,3 +18,57 @@ gsap.fromTo(".menu li",
     opacity: 1,
     stagger: 0.5
 })
+
+const carousel = document.querySelector('.carousel');
+const list = carousel.querySelector('.list');
+const thumbs = carousel.querySelector('.thumbnail');
+
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+
+let isAnimating = false;
+
+function setActive() {
+    list.querySelectorAll('.item').forEach(i => i.classList.remove('active'));
+    thumbs.querySelectorAll('.item').forEach(i => i.classList.remove('active'));
+
+    list.children[0].classList.add('active');
+    thumbs.children[0].classList.add('active');
+}
+
+setActive();
+
+function nextSlide() {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    carousel.classList.add('next');
+
+    setTimeout(() => {
+        list.appendChild(list.firstElementChild);
+        thumbs.appendChild(thumbs.firstElementChild);
+
+        carousel.classList.remove('next');
+        setActive();
+        isAnimating = false;
+    }, 500);
+}
+
+function prevSlide() {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    carousel.classList.add('prev');
+
+    setTimeout(() => {
+        list.prepend(list.lastElementChild);
+        thumbs.prepend(thumbs.lastElementChild);
+
+        carousel.classList.remove('prev');
+        setActive();
+        isAnimating = false;
+    }, 500);
+}
+
+nextBtn.onclick = nextSlide;
+prevBtn.onclick = prevSlide;
